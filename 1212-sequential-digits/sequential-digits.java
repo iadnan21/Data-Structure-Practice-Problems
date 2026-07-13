f6e562e5f60cffb53a05b7205f2f495b1300a1aa
Adnan <adnanali8808@gmail.com>
2026-07-13 18:03:43 +0530

    Time: 1 ms (28.06%) | Memory: 42.4 MB (56.92%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> answer = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        // BFS approach
+        for (int i = 1; i <= 9; i++) {
+            queue.offer(i);
+        }
+
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+
+            if (num >= low && num <= high) {
+                answer.add(num);
+            }
+            int lastDigit = num % 10;
+
+            if (lastDigit == 9) {
+                continue;
+            }
+
+            int nextDigit = num * 10 + (lastDigit + 1);
+
+            if (nextDigit <= high) {
+                queue.offer(nextDigit);
+            }
+        }
+
+        return answer;
+    }
+}
