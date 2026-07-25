5d4238c2aba0aa7ee011c0cf6675eab87fcebb70
Adnan <adnanali8808@gmail.com>
2026-07-25 13:01:08 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (52.94%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.go
@@ -0,0 +1,16 @@
+func maxProduct(n int) int {
+	max1 := -1
+	max2 := -1
+
+	for n != 0 {
+		rem := n % 10
+		if max1 < rem {
+			max2 = max1
+			max1 = rem
+		} else if max2 < rem {
+			max2 = rem
+		}
+		n /= 10
+	}
+	return max1 * max2
+}
